7999407449a657b2e35b216600b4498744a14e64
ychun <ychun@littlepenguinychun.myguest.virtualbox.org>
2024-07-07 01:33:59 +0200

    00 01 02 done

---
+++ assignment01/main.c
@@ -0,0 +1,19 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("ychun");
+MODULE_DESCRIPTION("ychun made for hello to world :)");
+
+static int __init hello_init(void) {
+	printk(KERN_INFO "Hello world!\n");
+	return 0;
+}
+
+static void __exit hello_exit(void) {
+	printk(KERN_INFO "Cleaning up module.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
